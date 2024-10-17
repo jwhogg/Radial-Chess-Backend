9187ddcf64fd528da1d5b325a94400e2fbdd7f4b
Joel Hogg <joelhogg45@gmail.com>
2024-10-17 12:38:23 +0100

    minors to testing code

--- src/testing.rs
+++ src/testing.rs
@@ -1,9 +1,17 @@
-use axum::{response::{IntoResponse, Json}, http::StatusCode};
+use axum::{response::{IntoResponse, Json}, http::{StatusCode, Request}};
 use serde_json::json;
+use crate::authlayer;
 
-pub async fn test_setup() -> impl IntoResponse {
-    let response = json!({
-        "message": "Function triggered successfully",
-    });
-    (StatusCode::OK, Json(response))
+pub async fn test_setup(req: Request<hyper::Body>) -> impl IntoResponse {
+    let response = match authlayer::get_jwt_sub(&req).await {
+        Ok(id) => (StatusCode::OK, Json(json!({
+            "message": "Function triggered successfully",
+            "user_id": id //dont actually use this function, we dont want to expose id to the client
+        }))),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({
+            "message": format!("Ran into problem getting the token from JWT: {:?}", e)
+        })))
+    };
+    
+    response
 }
